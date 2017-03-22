abba121dde1a117e605198328c0dae004be153e1
nguyenductho <nguyenductho89@gmail.com>
2017-03-22 19:47:25 +0700

    Change program

--- 02.practical.work.gethostbyname.c
+++ 02.practical.work.gethostbyname.c
@@ -9,9 +9,31 @@
 #include <stdio.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-int main(){
-    struct hostent* h;
-    h = gethostbyname("google.com.vn");
-    printf( "IP Address: %s ", inet_ntoa( *( struct in_addr*)( h -> h_addr_list[0])));
+//int main(){
+//    struct hostent* h;
+//    h = gethostbyname("google.com.vn");
+//    printf( "IP Address: %s ", inet_ntoa( *( struct in_addr*)( h -> h_addr_list[0])));
+//    return 0;
+//}
+int main( int argc, char *argv[] ) {
+    int i;
+    
+    char hostName[100];
+    
+    if ( argc == 1 ) {
+        printf("Nhap hostname\n");
+        scanf("%s",hostName);
+            struct hostent* h;
+            h = gethostbyname(hostName);
+        printf( "IPAddress of :%s",hostName );
+            printf( "\n %s\n ", inet_ntoa( *( struct in_addr*)( h -> h_addr_list[0])));
+    } else {
+        printf( "IPAddress of :%s",argv[1] );
+        
+            struct hostent* h;
+            h = gethostbyname(argv[1]);
+            printf( "\n%s\n ", inet_ntoa( *( struct in_addr*)( h -> h_addr_list[0])));
+    }
+    
     return 0;
 }
